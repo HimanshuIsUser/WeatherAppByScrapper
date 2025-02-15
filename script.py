65c2c07611eb1f4a851974e91898acc2cbe79469
himanshulsuser <himanshub166@gmail.com>
2025-02-15 17:56:28 +0530

    targeted api and list of cities

--- script.py
+++ script.py
@@ -1,7 +1,24 @@
+import requests
+
 class WeatherApp:
     def __init__(self):
-        pass
+        self.cities = [
+            "New York City", "London", "Paris", "Tokyo", "Sydney", 
+            "Dubai", "Rome", "Los Angeles", "Beijing", "Rio de Janeiro",
+            "Toronto", "Singapore", "Moscow", "Cape Town", "Bangkok",
+            "Barcelona", "Berlin", "Istanbul", "Hong Kong", "Amsterdam",
+            "San Francisco", "Seoul", "Mumbai", "Shanghai", "Vienna",
+            "Madrid", "Chicago", "Lisbon", "Kuala Lumpur", "Prague"
+        ]
+        self.api = "https://api.openweathermap.org/data/2.5/"
+        self.api_token = "appid=5796abbde9106b7da4febfae8c44c232"
+    
 
+    def _request_for_cities_details_(self,city_name):
+        request = requests.get(f'{self.api}find?d={city_name}&{self.api_token}')
+        return request
+    
+    
 def main():
     pass
 if __name__=='__main__':
