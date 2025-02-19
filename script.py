c4ee5cec1d6fd4a3ea49bea04be9f8638c73fa00
himanshulsuser <himanshub166@gmail.com>
2025-02-19 14:48:54 +0530

    endpoint tested with 200 response

--- script.py
+++ script.py
@@ -3,27 +3,22 @@ import pdb
 class WeatherApp:
     def __init__(self):
         self.cities = [
-            "New York City", "London", "Paris", "Tokyo", "Sydney", 
-            "Dubai", "Rome", "Los Angeles", "Beijing", "Rio de Janeiro",
-            "Toronto", "Singapore", "Moscow", "Cape Town", "Bangkok",
-            "Barcelona", "Berlin", "Istanbul", "Hong Kong", "Amsterdam",
-            "San Francisco", "Seoul", "Mumbai", "Shanghai", "Vienna",
-            "Madrid", "Chicago", "Lisbon", "Kuala Lumpur", "Prague"
+            "Delhi"
         ]
         self.api = "https://api.openweathermap.org/data/2.5/"
         self.api_token = "appid=5796abbde9106b7da4febfae8c44c232"
     
 
     def _request_for_cities_details_(self,city_name):
-        request = requests.get(f'{self.api}find?d={city_name}&{self.api_token}')
-        pdb.set_trace()
+        request = requests.get(f'{self.api}find?q={city_name}&{self.api_token}')
+        print(request.url)
         return request
     
     
 def main():
     initialize_class = WeatherApp()
     for i in initialize_class.cities:
-        initialize_class._request_for_cities_details_(i)
-
+        result = initialize_class._request_for_cities_details_(i)
+        print(result)
 if __name__=='__main__':
     main()
