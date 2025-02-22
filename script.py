02d293a2754d778761bb0465f2ac8a590a214e84
himanshulsuser <himanshub166@gmail.com>
2025-02-23 02:55:58 +0530

    method to store data in csv

--- script.py
+++ script.py
@@ -3,6 +3,8 @@ import pdb
 import json
 import os
 import datetime
+import csv
+
 class WeatherApp:
     def __init__(self):
         self.cities = [
@@ -10,6 +12,7 @@ class WeatherApp:
         ]
         self.api = "https://api.openweathermap.org/data/2.5/"
         self.api_token = "appid=5796abbde9106b7da4febfae8c44c232"
+        self.file_name = f'{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
     
 
     def _request_for_cities_details_(self,city_name):
@@ -17,15 +20,26 @@ class WeatherApp:
         print(request.url)
         return request
     
+    def _store_in_json_file(self,data):
+        file_name = f'{self.file_name}.json'
+        dump = json.dumps(data.json(), indent=4)
+        with open(file_name,'a') as file:
+            file.write(dump)
+            file.close()
+
+    def _store_in_csv_file(self,data):
+        file_name = f'{self.file_name}.csv'
+        with open(file_name,'a') as file:
+            csv_data = csv.DictWriter(file,fieldnames=['date','city','latitude','longitude','temprature'])
+            csv_data.writeheader()
+            csv_data.writerows({})
+            
     
 def main():
     initialize_class = WeatherApp()
     for i in initialize_class.cities:
         result = initialize_class._request_for_cities_details_(i)
-        file_name = f'{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.json'
-        dump = json.dumps(result.json(), indent=4)
-        with open(file_name,'a') as file:
-            file.write(dump)
-            file.close()
+        data_in_json = initialize_class._store_in_csv_file(result)
+        
 if __name__=='__main__':
     main()
